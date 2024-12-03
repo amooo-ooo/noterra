04a913fc0e3e855a0b7fb52d258ff1e05cacb021
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2024-12-04 11:14:58 +1300

    a11y and debugging improvements for icons

--- src/app/components/icons.tsx
+++ src/app/components/icons.tsx
@@ -1,29 +1,53 @@
 "use client";
-import 'material-symbols';
 
-export const Icon = ({ codePoint = null, iconStyle = 'outlined', ...props }: React.HTMLAttributes<HTMLSpanElement> & {
-    codePoint?: string | null;
-    iconStyle?: 'outlined' | 'rounded' | 'sharp';
-}) => (<span className={`material-symbols-${iconStyle}`} {...props}>{codePoint}</span>);
+import "material-symbols";
+import type React from "react";
 
-export const createIcon = (codePoint: string) => (props: React.HTMLAttributes<HTMLSpanElement>) =>
-    <Icon codePoint={codePoint} {...props} />;
+export type IconProps = React.HTMLAttributes<HTMLSpanElement> & {
+	codePoint?: string;
+	label: string;
+	iconStyle?: "outlined" | "rounded" | "sharp";
+};
 
-export const Close = createIcon('\ue5cd');
-export const Add = createIcon('\ue145');
-export const Remove = createIcon('\ue15b');
-export const FormatBold = createIcon('\ue238');
-export const FormatItalic = createIcon('\ue23f');
-export const FormatUnderline = createIcon('\ue249');
-export const FormatStrikethrough = createIcon('\ue246');
-export const Superscript = createIcon('\uf112');
-export const Subscript = createIcon('\uf111');
-export const FormatListBulleted = createIcon('\ue241');
-export const FormatListNumbered = createIcon('\ue242');
-export const Code = createIcon('\ue86f');
-export const CodeBlocks = createIcon('\uf84d');
-export const FormatQuote = createIcon('\ue244');
-export const Undo = createIcon('\ue166');
-export const Redo = createIcon('\ue15a');
-export const Link = createIcon('\ue157');
-export const HorizontalRule = createIcon('\uf108');
+export const Icon = ({
+	codePoint,
+	label,
+	iconStyle = "outlined",
+	...props
+}: IconProps) => (
+	<span
+		className={`material-symbols-${iconStyle}`}
+		role="img"
+		aria-label={label.replace(/([a-z])([A-Z])/g, "$1 $2")}
+		{...props}
+	>
+		{codePoint ?? label}
+	</span>
+);
+
+const createIcon = (name: string, codePoint: string) => {
+	const component: React.FC = (
+		props: React.HTMLAttributes<HTMLSpanElement>,
+	) => <Icon codePoint={codePoint} label={name} {...props} />;
+	component.displayName = `${name}Icon`;
+	return component;
+};
+
+export const Close = createIcon("Close", "\ue5cd");
+export const Add = createIcon("Add", "\ue145");
+export const Remove = createIcon("Remove", "\ue15b");
+export const FormatBold = createIcon("FormatBold", "\ue238");
+export const FormatItalic = createIcon("FormatItalic", "\ue23f");
+export const FormatUnderline = createIcon("FormatUnderline", "\ue249");
+export const FormatStrikethrough = createIcon("FormatStrikethrough", "\ue246");
+export const Superscript = createIcon("Superscript", "\uf112");
+export const Subscript = createIcon("Subscript", "\uf111");
+export const FormatListBulleted = createIcon("FormatListBulleted", "\ue241");
+export const FormatListNumbered = createIcon("FormatListNumbered", "\ue242");
+export const Code = createIcon("Code", "\ue86f");
+export const CodeBlocks = createIcon("CodeBlocks", "\uf84d");
+export const FormatQuote = createIcon("FormatQuote", "\ue244");
+export const Undo = createIcon("Undo", "\ue166");
+export const Redo = createIcon("Redo", "\ue15a");
+export const Link = createIcon("Link", "\ue157");
+export const HorizontalRule = createIcon("HorizontalRule", "\uf108");
