813b52e73078d1c706eae6ef96a094e34f123f90
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2024-12-04 11:24:09 +1300

    css tweaks, todos

--- src/app/components/editor-toolbar.tsx
+++ src/app/components/editor-toolbar.tsx
@@ -5,38 +5,135 @@ import { ReactSortable } from "react-sortablejs";
 import { unreachable } from "./util";
 import { TiptapButton } from "./tiptap-fields";
 
-import { 
-	FormatBold, 
-	FormatItalic, 
-	FormatUnderline, 
-	FormatStrikethrough, 
-	Code, 
-	CodeBlocks, 
-	Superscript, 
-	Subscript, 
-	FormatListNumbered, 
+import {
+	FormatBold,
+	FormatItalic,
+	FormatUnderline,
+	FormatStrikethrough,
+	Code,
+	CodeBlocks,
+	Superscript,
+	Subscript,
+	FormatListNumbered,
 	Undo,
 	Redo,
 	FormatListBulleted,
 	HorizontalRule,
-	FormatQuote
+	FormatQuote,
 } from "./icons";
 
 const TOOLS = {
-	bold: <TiptapButton label="bold" action={ctx => ctx.toggleBold()} detect={ed => ed.isActive('bold')} icon={<FormatBold />} />,
-	italic: <TiptapButton label="italic" action={ctx => ctx.toggleItalic()} detect={ed => ed.isActive('italic')} icon={<FormatItalic />} />,
-	underline: <TiptapButton label="underline" action={ctx => ctx.toggleUnderline()} detect={ed => ed.isActive('underline')} icon={<FormatUnderline />} />,
-	strikethrough: <TiptapButton label="strikethrough" action={ctx => ctx.toggleStrike()} detect={ed => ed.isActive('strike')} icon={<FormatStrikethrough />} />,
-	code: <TiptapButton label="code" action={ctx => ctx.toggleCode()} detect={ed => ed.isActive('code')} icon={<Code />} />,
-	codeblock: <TiptapButton label="codeblock" action={ctx => ctx.toggleCodeBlock()} detect={ed => ed.isActive('codeblock')} icon={<CodeBlocks />} />,
-	blockquote: <TiptapButton label="blockquote" action={ctx => ctx.toggleBlockquote()} detect={ed => ed.isActive('blockquote')} icon={<FormatQuote />} />,
-	superscript: <TiptapButton label="superscript" action={ctx => ctx.toggleSuperscript()} detect={ed => ed.isActive('superscript')} icon={<Superscript />} />,
-	subscript: <TiptapButton label="subscript" action={ctx => ctx.toggleSuperscript()} detect={ed => ed.isActive('subscript')} icon={<Subscript />} />,
-	orderedlist: <TiptapButton label="orderedlist" action={ctx => ctx.toggleOrderedList()} detect={ed => ed.isActive('orderedList')} icon={< FormatListNumbered />} />,
-	bulletList: <TiptapButton label="bulletList" action={ctx => ctx.toggleBulletList()} detect={ed => ed.isActive('bulletList')} icon={<FormatListBulleted />} />,
-	horizontalRule: <TiptapButton label="bulletList" action={ctx => ctx.setHorizontalRule()} icon={<HorizontalRule />} />,
-	undo: <TiptapButton label="undo" action={ctx => ctx.undo()} tiptapDisabled={ctx => !ctx.can().undo()} icon={<Undo />} />,
-	redo: <TiptapButton label="redo" action={ctx => ctx.redo()} tiptapDisabled={ctx => !ctx.can().redo()} icon={< Redo />} />,
+	bold: (
+		<TiptapButton
+			label="bold"
+			action={(ctx) => ctx.toggleBold()}
+			detect={(ed) => ed.isActive("bold")}
+			icon={<FormatBold />}
+		/>
+	),
+	italic: (
+		<TiptapButton
+			label="italic"
+			action={(ctx) => ctx.toggleItalic()}
+			detect={(ed) => ed.isActive("italic")}
+			icon={<FormatItalic />}
+		/>
+	),
+	underline: (
+		<TiptapButton
+			label="underline"
+			action={(ctx) => ctx.toggleUnderline()}
+			detect={(ed) => ed.isActive("underline")}
+			icon={<FormatUnderline />}
+		/>
+	),
+	strikethrough: (
+		<TiptapButton
+			label="strikethrough"
+			action={(ctx) => ctx.toggleStrike()}
+			detect={(ed) => ed.isActive("strike")}
+			icon={<FormatStrikethrough />}
+		/>
+	),
+	code: (
+		<TiptapButton
+			label="code"
+			action={(ctx) => ctx.toggleCode()}
+			detect={(ed) => ed.isActive("code")}
+			icon={<Code />}
+		/>
+	),
+	codeblock: (
+		<TiptapButton
+			label="codeblock"
+			action={(ctx) => ctx.toggleCodeBlock()}
+			detect={(ed) => ed.isActive("codeblock")}
+			icon={<CodeBlocks />}
+		/>
+	),
+	blockquote: (
+		<TiptapButton
+			label="blockquote"
+			action={(ctx) => ctx.toggleBlockquote()}
+			detect={(ed) => ed.isActive("blockquote")}
+			icon={<FormatQuote />}
+		/>
+	),
+	superscript: (
+		<TiptapButton
+			label="superscript"
+			action={(ctx) => ctx.toggleSuperscript()}
+			detect={(ed) => ed.isActive("superscript")}
+			icon={<Superscript />}
+		/>
+	),
+	subscript: (
+		<TiptapButton
+			label="subscript"
+			action={(ctx) => ctx.toggleSuperscript()}
+			detect={(ed) => ed.isActive("subscript")}
+			icon={<Subscript />}
+		/>
+	),
+	orderedlist: (
+		<TiptapButton
+			label="orderedlist"
+			action={(ctx) => ctx.toggleOrderedList()}
+			detect={(ed) => ed.isActive("orderedList")}
+			icon={<FormatListNumbered />}
+		/>
+	),
+	bulletList: (
+		<TiptapButton
+			label="bulletList"
+			action={(ctx) => ctx.toggleBulletList()}
+			detect={(ed) => ed.isActive("bulletList")}
+			icon={<FormatListBulleted />}
+		/>
+	),
+	horizontalRule: (
+		<TiptapButton
+			label="bulletList"
+			action={(ctx) => ctx.setHorizontalRule()}
+			icon={<HorizontalRule />}
+		/>
+	),
+	undo: (
+		<TiptapButton
+			label="undo"
+			action={(ctx) => ctx.undo()}
+			tiptapDisabled={(ctx) => !ctx.can().undo()}
+			icon={<Undo />}
+		/>
+	),
+	redo: (
+		<TiptapButton
+			label="redo"
+			action={(ctx) => ctx.redo()}
+			tiptapDisabled={(ctx) => !ctx.can().redo()}
+			icon={<Redo />}
+		/>
+	),
 	fontSize: <input type="number" />,
 	/// ...
 };
@@ -59,14 +156,17 @@ function toolbarDispatch(
 	state: ToolbarConfig,
 	action:
 		| {
-			type: "rearrange";
-			order: ((Pick<ToolbarGroup, "id"> & { isGroup: true }) | ToolbarItemTypes)[];
-		}
+				type: "rearrange";
+				order: (
+					| (Pick<ToolbarGroup, "id"> & { isGroup: true })
+					| ToolbarItemTypes
+				)[];
+		  }
 		| {
-			type: "rearrange-child";
-			childId: ToolbarGroup["id"];
-			order: ToolbarItemTypes[];
-		},
+				type: "rearrange-child";
+				childId: ToolbarGroup["id"];
+				order: ToolbarItemTypes[];
+		  },
 ): ToolbarConfig {
 	switch (action.type) {
 		case "rearrange": {
@@ -113,19 +213,37 @@ const ToolbarConfigContext = React.createContext<{
 }>(null!);
 
 export function ToolbarConfigProvider(props: React.PropsWithChildren<object>) {
+	// TODO: sortable needs fixing: broken state when dragging across sortables
 	const initGroups = [
-		['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'code', 'codeblock', 'blockquote', 'bulletList', 'orderedlist', 'horizontalRule', 'undo', 'redo'],
-		['fontSize'],
+		[
+			"bold",
+			"italic",
+			"underline",
+			"strikethrough",
+			"superscript",
+			"subscript",
+			"code",
+			"codeblock",
+			"blockquote",
+			"bulletList",
+			"orderedlist",
+			"horizontalRule",
+			"undo",
+			"redo",
+		],
+		["fontSize"],
 	] satisfies (keyof typeof TOOLS)[][];
 	const [arrangement, updateArrangement] = React.useReducer(toolbarDispatch, {
 		nextGroupId: initGroups.length,
 		groups: {
 			...initGroups.map((els, id) => ({
 				id,
-				items: els.map(el => ({ id: el })),
+				items: els.map((el) => ({ id: el })),
 			})),
 		},
-		order: new Array(initGroups.length).fill(null).map((_, i) => ({ id: i, isGroup: true })),
+		order: new Array(initGroups.length)
+			.fill(null)
+			.map((_, i) => ({ id: i, isGroup: true })),
 	} satisfies ToolbarConfig);
 	return (
 		<ToolbarConfigContext.Provider
