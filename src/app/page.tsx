9f7141b817a0cd1b9f45c955b69c7daa5b29d4ac
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2024-12-04 11:00:36 +1300

    EditorSelectionHandler should only wrap editor content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,23 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { TabManager } from "./components/tab-manager";
 import { ToolbarConfigProvider } from "./components/editor-toolbar";
 import { EditorSelectionHandler } from "./components/monaco-editor";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <EditorSelectionHandler>
-          <div className={styles.sidebar} />
-          <div className={styles.editor}>
-            <ToolbarConfigProvider>
-              <TabManager />
-            </ToolbarConfigProvider>
-          </div>
-          <div className={styles.sidebar} />
-        </EditorSelectionHandler>
-      </main>
-      <footer className={styles.footer}>
-      </footer>
-    </div>
-  );
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<div className={styles.sidebar} />
+				<div className={styles.editor}>
+					<EditorSelectionHandler>
+						<ToolbarConfigProvider>
+							<TabManager />
+						</ToolbarConfigProvider>
+					</EditorSelectionHandler>
+				</div>
+				<div className={styles.sidebar} />
+			</main>
+			<footer className={styles.footer} />
+		</div>
+	);
 }
