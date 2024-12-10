01c2b46e6e85df99dabe41bb7ebc1512da24f9f1
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2024-12-11 11:02:04 +1300

    fix indentation with multiple lines

--- src/app/components/indent.ts
+++ src/app/components/indent.ts
@@ -27,50 +27,52 @@ export const Indent = Extension.create<IndentOptions>({
 
 	addCommands() {
 		const adjustIndent =
-			(adjustment: (text: string) => string, cursorOffset: number) =>
+			(dedent: boolean) =>
 			({ tr, state, dispatch }: CommandProps) => {
-				const { selection, schema, doc } = state;
+				const { selection, doc } = state;
 				const { from, to, anchor, head } = selection;
+				const dir = dedent ? -1 : 1;
 
-				let modified = false;
+				let modified = 0;
 
 				doc.nodesBetween(from, to, (node, pos) => {
 					if (this.options.types.includes(node.type.name)) {
-						const originalText = node.textContent ?? "";
-						const newText = adjustment(originalText);
-
-						if (newText === originalText || newText === "") return;
-
-						tr.replaceWith(
-							pos + 1,
-							pos + node.nodeSize - 1,
-							schema.text(newText),
-						);
-						modified = true;
+						if (dedent) {
+							if (node.textContent.startsWith("\t"))
+								tr.replaceWith(
+									pos + 1 + modified * dir,
+									pos + 2 + modified * dir,
+									[],
+								);
+							else return;
+						} else tr.insertText("\t", pos + 1 + modified * dir);
+						modified++;
 					}
 				});
 
 				if (modified && dispatch) {
 					tr.setSelection(
-						TextSelection.create(
-							tr.doc,
-							anchor + cursorOffset,
-							head + cursorOffset,
-						),
+						anchor > head
+							? TextSelection.create(
+									tr.doc,
+									anchor + modified * dir,
+									head + dir,
+								)
+							: TextSelection.create(
+									tr.doc,
+									anchor + dir,
+									head + modified * dir,
+								),
 					);
 					dispatch(tr);
 				}
 
-				return modified;
+				return !!modified;
 			};
 
 		return {
-			indent: () => adjustIndent((text) => `\t${text}`, 1),
-			outdent: () =>
-				adjustIndent(
-					(text) => (text.startsWith("\t") ? text.slice(1) : text),
-					-1,
-				),
+			indent: () => adjustIndent(false),
+			outdent: () => adjustIndent(true),
 			increaseIndent:
 				() =>
 				({ commands }: CommandProps) =>
