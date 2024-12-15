b9e73b0719b545fd89128fd1af52ec8d43f9bfd8
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2024-12-15 22:39:16 +1300

    preserve tab states

--- src/app/components/tab-manager.tsx
+++ src/app/components/tab-manager.tsx
@@ -31,24 +31,61 @@ type TabReducerAction<T> =
 			value: T[];
 	  };
 
-function tabReducer<T>(state: T[], action: TabReducerAction<T>) {
+function tabReducer(state: TabData[], action: TabReducerAction<TabData>) {
+	console.log(state, action);
+	// TODO: dont just do async race condition like
 	switch (action.type) {
 		case "append":
+			(async () => {
+				const editors = await LocalFile.db.openStore("editors", "readwrite");
+				editors.add(action.initialValue.serialize(state.length));
+			})();
 			return state.concat(action.initialValue);
 		case "insert":
+			(async () => {
+				const editors = await LocalFile.db.openStore("editors", "readwrite");
+				editors.add(action.initialValue.serialize(action.index));
+				for (let i = action.index; i < state.length; i++) {
+					editors.put(state[i].serialize(i + 1));
+				}
+			})();
 			return [
 				...state.slice(0, action.index),
 				action.initialValue,
-				...state.slice(action.index + 1),
+				...state.slice(action.index),
 			];
 		case "remove":
+			(async () => {
+				const editors = await LocalFile.db.openStore("editors", "readwrite");
+				let offset = 0;
+				for (let i = 0; i < state.length; i++) {
+					if (action.predicate(state[i])) {
+						editors.delete(state[i].id);
+						state[i].file.save();
+						offset++;
+					} else if (offset) {
+						editors.put(state[i].serialize(i - offset));
+					}
+				}
+			})();
 			return state.filter((x) => !action.predicate(x));
 		case "mutate": {
 			const newState = [...state];
 			newState[action.index] = action.modify(newState[action.index]);
+			(async () => {
+				const editors = await LocalFile.db.openStore("editors", "readwrite");
+				editors.put(newState[action.index].serialize(action.index));
+			})();
 			return newState;
 		}
 		case "reorder":
+			(async () => {
+				const editors = await LocalFile.db.openStore("editors", "readwrite");
+				editors.clear();
+				for (let i = 0; i < action.value.length; i++) {
+					editors.put(action.value[i].serialize(i));
+				}
+			})();
 			return action.value;
 		default:
 			unreachable(action);
@@ -74,18 +111,20 @@ export function TabManager({
 	statsWidgetClass?: string;
 	editorClass?: string;
 }) {
-	const [tabs, modifyTabs] = React.useReducer(tabReducer<TabData>, []);
+	const [tabs, modifyTabs] = React.useReducer(tabReducer, []);
 	const [currentTab, setCurrentTab] = React.useState<TabData["id"]>();
 	const [nextId, setNextId] = React.useState("0");
 
+	// biome-ignore lint/correctness/useExhaustiveDependencies: intentional
 	React.useEffect(() => {
 		(async () => {
+			const tempTabs = tabs;
 			for await (const tab of LocalFile.editors()) {
-				let index = Math.round(tabs.length / 2);
+				let index = Math.floor(tempTabs.length / 2);
 				let shift = index;
 				while (shift) {
-					shift = Math.round(shift / 2);
-					const rIndex = tabs[index].tryIndex;
+					shift = Math.floor(shift / 2);
+					const rIndex = tempTabs[index].tryIndex;
 					index += shift * (rIndex && tab.tryIndex > rIndex ? 1 : -1);
 				}
 				modifyTabs({
@@ -93,13 +132,14 @@ export function TabManager({
 					index,
 					initialValue: tab,
 				});
+				tempTabs.splice(index, 0, tab);
 				// TODO: replace with better id system
 				setNextId(
 					`${Math.max(Number.parseInt(nextId), Number.parseInt(`0${tab.file.id}`) + 1)}`,
 				);
 			}
 		})();
-	});
+	}, []);
 
 	return (
 		<>
@@ -107,8 +147,8 @@ export function TabManager({
 				{...{ tabs, modifyTabs, currentTab, setCurrentTab }}
 				idGen={() => {
 					const id = nextId;
-					setNextId(id + 1);
-					return `${id}`;
+					setNextId(`${Number.parseInt(id) + 1}`);
+					return id;
 				}}
 				className={tabstripClass}
 				atStripEnd={<ThemeButton size="1.5em" />}
