b403c0757725b3725511baacb53e47cd67ea87a9
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2024-12-16 15:29:47 +1300

    dont make deadend requests for blob sources

--- src/app/editor-extensions/blob-imgs.ts
+++ src/app/editor-extensions/blob-imgs.ts
@@ -10,15 +10,16 @@ export const BlobImages = Image.extend({
 			isBlob: {
 				default: false,
 				parseHTML: (el) => !!el.dataset.blobSrc,
-				renderHTML: (attrs) =>
-					attrs.isBlob ? { "data-blob-src": attrs.src } : { src: attrs.src },
+				// renderHTML: (attrs) =>
+				// 	attrs.isBlob ? { "data-blob-src": attrs.src } : { src: attrs.src },
 			},
 			src: {
 				...("src" in attrs ? (attrs.src as object) : {}),
 				parseHTML(el) {
 					return el.dataset.blobSrc ?? (el as HTMLImageElement).src;
 				},
-				// renderHTML: attrs => attrs.isBlob ? { 'data-blob-src': attrs.src } : { src: attrs.src },
+				renderHTML: (attrs) =>
+					attrs.isBlob ? { "data-blob-src": attrs.src } : { src: attrs.src },
 			},
 		};
 	},
