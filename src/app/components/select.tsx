ec607a6d2f41400ed892ca4ab07685f584fb451c
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2024-12-26 11:44:52 +1300

    dont hardcode padding

--- src/app/components/select.tsx
+++ src/app/components/select.tsx
@@ -379,7 +379,7 @@ export const SelectPopover = React.forwardRef<HTMLDivElement, PopoverProps>(
 					className={styles["popout-scroll-container"]}
 					style={
 						typeof display === "object" && display.type === "grid"
-							? { gridTemplateColumns: `repeat(${display.width * 2}, auto)`, padding: "2px" }
+							? { gridTemplateColumns: `repeat(${display.width * 2}, auto)` }
 							: {}
 					}
 					ref={ref}
